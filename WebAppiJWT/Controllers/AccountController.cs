4f6755c6005c008effe7e24aae9f4efcde763a15
Entokas <tunahancin@hotmail.com>
2021-09-09 20:18:41 +0300

    WebAppiJWT
    Connecting MVC project with JWT Web API

---
+++ WebAppiJWT/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+
+namespace WebAppiJWT.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        // GET: Accounts
+        public HttpResponseMessage ValidLogin(string userName, string userPassword)
+        {
+            if (userName == "admin"  && userPassword == "admin")
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, value: TokenManager.GenerateToken(userName));
+
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, message: "User name and  password is invalid");
+            }
+        }
+        [HttpGet]
+        [CustomerAuthenticationFilter]
+        public HttpResponseMessage GetEmployee()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, value: "Succesfully Valid");
+        }
+
+    }
+}
---
+++ WebAppiJWT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppiJWT.WebApiApplication" Language="C#" %>
